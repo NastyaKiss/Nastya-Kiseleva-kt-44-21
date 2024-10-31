63aaa02ca6f0e03ef150766cfc788ac860d5027b
kiseleva-anastasia-kt-44-21 <nastya.kiseleva.234@list.ru>
2024-10-31 17:35:11 +0300

    add logging and WeatherForecast test request

--- AnastasiaKiselevaKt-44-21/Program.cs
+++ AnastasiaKiselevaKt-44-21/Program.cs
@@ -1,8 +1,15 @@
+using NLog;
+using NLog.Web;
+
 var builder = WebApplication.CreateBuilder(args);
+var logger = LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
 
-// Add services to the container.
-
-builder.Services.AddControllers();
+try
+{
+    builder.Logging.ClearProviders();
+    builder.Host.UseNLog();
+    // Add services to the container.
+    builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -21,3 +28,12 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+}
+catch (Exception ex)
+{
+    logger.Error(ex, "Stopped program because of exception");
+}
+finally
+{
+    LogManager.Shutdown();
+}
