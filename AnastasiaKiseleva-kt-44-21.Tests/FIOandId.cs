c5c989d09557276e52b0d4fcdb8f6291c1ae6701
kiseleva-anastasia-kt-44-21 <nastya.kiseleva.234@list.ru>
2024-11-15 15:19:54 +0300

    6laba(povtor)

--- AnastasiaKiseleva-kt-44-21.Tests/FIOandId.cs
+++ AnastasiaKiseleva-kt-44-21.Tests/FIOandId.cs
@@ -10,42 +10,51 @@ using System.Threading.Tasks;
 
 namespace AnastasiaKiselevaKt_44_21.Tests
 {
-    public class FIOandId
+    public class StudentTests : IDisposable
     {
-        public readonly DbContextOptions<StudentDbContext> _dbContextOptions;
+        private readonly DbContextOptions<StudentDbContext> _dbContextOptions;
+        private readonly StudentDbContext _context;
+        private readonly StudentService _studentService;
 
-        public FIOandId()
+        public StudentTests()
         {
             _dbContextOptions = new DbContextOptionsBuilder<StudentDbContext>()
-                .UseInMemoryDatabase(databaseName: "studentfio_db_an")
+                .UseInMemoryDatabase(databaseName: "student_db_an") 
                 .Options;
+
+            _context = new StudentDbContext(_dbContextOptions);
+            _studentService = new StudentService(_context);
+
+            SeedDatabase(); 
+        }
+
+        private void SeedDatabase() // база данных
+        {
+            var groups = new List<Group>
+        {
+            new Group { GroupName = "KT-44-21" },
+            new Group { GroupName = "KT-43-21" },
+            new Group { GroupName = "KT-42-21" },
+            new Group { GroupName = "KT-41-21" }
+        };
+
+            _context.Set<Group>().AddRange(groups);
+
+            var students = new List<Student>
+        {
+            new Student { FirstName = "a", LastName = "a", MiddleName = "a", GroupId = 4 },
+            new Student { FirstName = "a", LastName = "a", MiddleName = "b", GroupId = 4 },
+            new Student { FirstName = "a", LastName = "b", MiddleName = "a", GroupId = 3 }
+        };
+
+            _context.Set<Student>().AddRange(students);
+            _context.SaveChanges();
         }
 
         [Fact]
         public async Task GetStudentsByFioAsync_KT4421_TwoObjects() // тест по ФИО 
         {
             // Arrange
-            var ctx = new StudentDbContext(_dbContextOptions);
-            var studentService = new StudentService(ctx);
-            var groups = new List<Group>
-            {
-                new Group { GroupName = "KT-44-21" },
-                new Group { GroupName = "KT-43-21" },
-                new Group { GroupName = "KT-42-21" },
-                new Group {GroupName = "KT-41-21"}
-            };
-            await ctx.Set<Group>().AddRangeAsync(groups);
-
-            var students = new List<Student>
-            {
-                new Student { FirstName = "a", LastName = "a", MiddleName = "a", GroupId = 4 },
-                new Student { FirstName = "a", LastName = "a", MiddleName = "b", GroupId = 4 },
-                new Student { FirstName = "a", LastName = "b", MiddleName = "a", GroupId = 4 }
-            };
-            await ctx.Set<Student>().AddRangeAsync(students);
-            await ctx.SaveChangesAsync();
-
-            // Act
             var filter1 = new Filters.StudentFioFilters.StudentFioFilter
             {
                 FirstName = "a",
@@ -53,55 +62,37 @@ namespace AnastasiaKiselevaKt_44_21.Tests
                 MiddleName = "a"
             };
 
-            var studentsResult1 = await studentService.GetStudentsByFioAsync(filter1, CancellationToken.None);
+            // Act
+            var studentsResult1 = await _studentService.GetStudentsByFioAsync(filter1, CancellationToken.None);
+
+            // Assert
             Assert.Equal(1, studentsResult1.Length);
         }
 
-        public class GroupId
+        [Fact]
+        public async Task GetStudentsByIdGroupAsync_KT4421_TwoObjects() // тест по GroupId 
         {
-            public readonly DbContextOptions<StudentDbContext> _dbContextOptions;
-
-            public GroupId()
+            // Act
+            var filter2 = new Filters.GroupFilter.StudentIdGroup
             {
-                _dbContextOptions = new DbContextOptionsBuilder<StudentDbContext>()
-                    .UseInMemoryDatabase(databaseName: "studentid_db_an")
-                    .Options;
-            }
-
-            [Fact]
-            public async Task GetStudentsByIdGroupAsync_KT4421_TwoObjects() // тест по GroupId 
-            {
-                var ctx = new StudentDbContext(_dbContextOptions);
-                var studentService = new StudentService(ctx);
-                var groups = new List<Group>
-            {
-                new Group { GroupName = "KT-44-21" },
-                new Group { GroupName = "KT-43-21" },
-                new Group { GroupName = "KT-42-21" },
-                new Group {GroupName = "KT-41-21"}
+                GroupId = 4
             };
-                await ctx.Set<Group>().AddRangeAsync(groups);
 
-                var students = new List<Student>
+            var studentsResult2 = await _studentService.GetStudentsByIdGroupAsync(filter2, CancellationToken.None);
+
+            // Assert
+            Assert.Equal(2, studentsResult2.Length);
+        }
+
+        public void Dispose()
+        {
+            // Удаление базы данных после тестов
+            using (var context = new StudentDbContext(_dbContextOptions))
             {
-                new Student { FirstName = "a", LastName = "a", MiddleName = "a", GroupId = 3 },
-                new Student { FirstName = "a", LastName = "a", MiddleName = "b", GroupId = 4 },
-                new Student { FirstName = "a", LastName = "b", MiddleName = "a", GroupId = 4 }
-            };
-                await ctx.Set<Student>().AddRangeAsync(students);
-                await ctx.SaveChangesAsync();
-
-
-                // Act
-                var filter2 = new Filters.GroupFilter.StudentIdGroup
-                {
-                    GroupId = 4
-                };
-                var studentsResult2 = await studentService.GetStudentsByIdGroupAsync(filter2, CancellationToken.None);
-
-                // Assert
-                Assert.Equal(2, studentsResult2.Length); // Проверяем, что 3 студента в группе с ID 3*/
+                context.Database.EnsureDeleted();
             }
         }
     }
 }
+    
+
